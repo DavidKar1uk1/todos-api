c9dd3787922612a29929a0bd86f4f360c0d25fc1
DavidKar1uk1 <David.mwangi@kopokop.com>
2019-01-25 16:01:37 +0300

    Update to Using Different Classes.

--- api_trial_one/app/controllers/todos_controller.rb
+++ api_trial_one/app/controllers/todos_controller.rb
@@ -44,8 +44,34 @@ class TodosController < ApplicationController
   # POST /parse
   def receive
     # test = JSON.parse(request.body.string).as_json
-    response.status = K2ConnectRubyApiGem::Client.new("#{ENV["K2_SECRET_KEY"].to_s}").parse_it_whole(request)
-    puts ("Response Status Code: #{response.code}\nResponse Message: #{response.message}")
+    #
+    # response.status = K2ConnectRuby::Client.new("#{ENV["K2_SECRET_KEY"].to_s}").parse_it_whole(request)
+    # puts ("Response Status Code: #{response.code}\nResponse Message: #{response.message}")
+    #
+    # Test Yajl
+    # test = Yajl::Parser.parse(request.body.string)
+    # puts(test["event"]["resource"][""])
+    #
+    k2_test = K2ConnectRuby::K2Client(ENV["K2_SECRET_KEY"])
+    k2_test.authorize_client(request)
+    # truth_value = K2ConnectRuby::K2Authorize.authenticate(k2_test.hash_body, k2_test.api_secret_key, k2_test.k2_signature)
+    # if truth_value
+    #   k2_components = K2ConnectRuby::K2SplitRequest.request_body_components(k2_test.hash_body)
+    #   puts(k2_components.topic)
+    #   return 200
+    # else
+    #   return 401
+    # end
+    # if k2_pasi.@hash_method.eql?("POST")
+    #   if K2ConnectRubyApiGem::K2SIGNATURE("#{ENV["K2_SECRET_KEY"].to_s}").authorize_it(k2_pasi.@hash_body, @hash_header)
+    #     K2ConnectRubyApiGem::K2SPLITREQ.assign_req_elements(JSON.parse(request.body.string).as_json) and return
+    #     return 200
+    #   else
+    #     return 401
+    #   end
+    # else
+    #   return 400
+    # end
   end
 
 end
